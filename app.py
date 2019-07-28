1226a62a6dc1fb414296327cc0d67c435b3f11e1
Miura55 <acordion.piano@gmail.com>
2019-07-28 23:35:26 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import datetime as dt
+import os
+import json
+import base64
+import urllib.parse
+import requests
+
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
+CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(CHANNEL_SECRET)
+
+app = Flask(__name__, static_folder='static')
+
+# DBを定義
+db_uri = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'userdatas'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False)
+    temperature = db.Column(db.Integer)
+    save_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __init__(self, username, heart_rate, save_date):
+        self.username = username
+        self.heart_rate = heart_rate
+        self.save_date = save_date
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def do_get():
+    return render_template('index.html')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body)
+
+    try:
+        # Python SDK doesn't support LINE Things event
+        # => Unknown event type. type=things
+        for event in parser.parse(body, signature):
+            handle_message(event)
+
+        # Parse JSON without SDK for LINE Things event
+        events = json.loads(body)
+        for event in events["events"]:
+            if "things" in event:
+                handle_things_event(event)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+def handle_things_event(event):
+    if event["things"]["type"] != "scenarioResult":
+        return
+    if event["things"]["result"]["resultCode"] != "success":
+        app.logger.warn("Error result: %s", event)
+        return
+
+    username = event["source"]["userId"]
+    heart_rate = int.from_bytes(base64.b64decode(event["things"]["result"]["bleNotificationPayload"]), 'little')
+    save_date = datetime.now()
+
+    print("Got data: " + str(heart_rate))
+    if heart_rate > 0:
+        user = User(username, heart_rate, save_date)
+        db.session.add(user)
+        db.session.commit()
+
+def handle_message(event):
+    if event.type == "message" and event.message.type == "text":
+        if event.message.text == "今の心拍数":
+            before_10s = datetime.now() - dt.timedelta(seconds=10)
+            userId = event.source.sender_id
+            users = db.session.query(User).filter(User.save_date>=before_10s).filter(User.username==userId).all()
+            list_heart = [int(data.heart_rate) for data in users]
+            try:
+                av_heart = sum(list_heart) / len(list_heart)
+                message = "現在の心拍: " + str(av_heart)
+            except ZeroDivisionError:
+                message = "デバイスの接続がありません。"
+        else:
+            message = event.message.text
+        line_bot_api.reply_message(event.reply_token,
+            TextSendMessage(text=message))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ static/js/liff.js
@@ -0,0 +1,208 @@
+// User service UUID: Change this to your generated service UUID
+const USER_SERVICE_UUID   = '25292c58-bd2a-448d-ad26-357f7855bed0';
+// User service characteristics
+const NOTIFY_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
+
+
+// PSDI Service UUID: Fixed value for Developer Trial
+const PSDI_SERVICE_UUID         = 'E625601E-9E55-4597-A598-76018A0D293D'; // Device ID
+const PSDI_CHARACTERISTIC_UUID  = '26E2B12B-85F0-4F3F-9FDD-91D114270E6E';
+
+
+// -------------- //
+// On window load //
+// -------------- //
+
+window.onload = () => {
+    initializeApp();
+};
+
+
+// ------------ //
+// UI functions //
+// ------------ //
+
+function uiToggleLedButton(state) {
+  const elStatus = document.getElementById("status");
+  const elControls = document.getElementById("controls");
+
+    if (state) {
+      el.classList.add("led-on");
+    } else {
+      el.classList.remove("led-on");
+    }
+}
+
+
+
+function uiToggleDeviceConnected(connected) {
+    const elStatus = document.getElementById("status");
+    const elControls = document.getElementById("controls");
+
+    elStatus.classList.remove("error");
+
+    if (connected) {
+        // Hide loading animation
+        uiToggleLoadingAnimation(false);
+        // Show status connected
+        elStatus.classList.remove("inactive");
+        elStatus.classList.add("success");
+        elStatus.innerText = "Device connected";
+        // Show controls
+        elControls.classList.remove("hidden");
+    } else {
+        // Show loading animation
+        uiToggleLoadingAnimation(true);
+        // Show status disconnected
+        elStatus.classList.remove("success");
+        elStatus.classList.add("inactive");
+        elStatus.innerText = "Device disconnected";
+        // Hide controls
+        elControls.classList.add("hidden");
+    }
+}
+
+function uiToggleLoadingAnimation(isLoading) {
+    const elLoading = document.getElementById("loading-animation");
+
+    if (isLoading) {
+        // Show loading animation
+        elLoading.classList.remove("hidden");
+    } else {
+        // Hide loading animation
+        elLoading.classList.add("hidden");
+    }
+}
+
+function uiStatusError(message, showLoadingAnimation) {
+    uiToggleLoadingAnimation(showLoadingAnimation);
+
+    const elStatus = document.getElementById("status");
+    const elControls = document.getElementById("controls");
+
+    // Show status error
+    elStatus.classList.remove("success");
+    elStatus.classList.remove("inactive");
+    elStatus.classList.add("error");
+    elStatus.innerText = message;
+
+    // Hide controls
+    elControls.classList.add("hidden");
+}
+
+function makeErrorMsg(errorObj) {
+    return "Error\n" + errorObj.code + "\n" + errorObj.message;
+}
+
+// -------------- //
+// LIFF functions //
+// -------------- //
+
+function initializeApp() {
+    liff.init(() => initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
+}
+
+function initializeLiff() {
+    liff.initPlugins(['bluetooth']).then(() => {
+        liffCheckAvailablityAndDo(() => liffRequestDevice());
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffCheckAvailablityAndDo(callbackIfAvailable) {
+    // Check Bluetooth availability
+    liff.bluetooth.getAvailability().then(isAvailable => {
+        if (isAvailable) {
+            uiToggleDeviceConnected(false);
+            callbackIfAvailable();
+        } else {
+            uiStatusError("Bluetooth not available", true);
+            setTimeout(() => liffCheckAvailablityAndDo(callbackIfAvailable), 10000);
+        }
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });;
+}
+
+function liffRequestDevice() {
+    liff.bluetooth.requestDevice().then(device => {
+        liffConnectToDevice(device);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffConnectToDevice(device) {
+    device.gatt.connect().then(() => {
+        document.getElementById("device-name").innerText = device.name;
+
+        // Show status connected
+        uiToggleDeviceConnected(true);
+
+        // Get service
+        device.gatt.getPrimaryService(USER_SERVICE_UUID).then(service => {
+            liffGetUserService(service);
+        }).catch(error => {
+            uiStatusError(makeErrorMsg(error), false);
+        });
+        device.gatt.getPrimaryService(PSDI_SERVICE_UUID).then(service => {
+            liffGetPSDIService(service);
+        }).catch(error => {
+            uiStatusError(makeErrorMsg(error), false);
+        });
+
+        // Device disconnect callback
+        const disconnectCallback = () => {
+            // Show status disconnected
+            uiToggleDeviceConnected(false);
+
+            // Remove disconnect callback
+            device.removeEventListener('gattserverdisconnected', disconnectCallback);
+
+            // Try to reconnect
+            initializeLiff();
+        };
+
+        device.addEventListener('gattserverdisconnected', disconnectCallback);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffGetUserService(service) {
+    // Button pressed state
+    service.getCharacteristic(NOTIFY_CHARACTERISTIC_UUID).then(characteristic => {
+        liffGetButtonStateCharacteristic(characteristic);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffGetPSDIService(service) {
+    // Get PSDI value
+    service.getCharacteristic(PSDI_CHARACTERISTIC_UUID).then(characteristic => {
+        return characteristic.readValue();
+    }).then(value => {
+        // Byte array to hex string
+        const psdi = new Uint8Array(value.buffer)
+            .reduce((output, byte) => output + ("0" + byte.toString(16)).slice(-2), "");
+        // document.getElementById("device-psdi").innerText = psdi;
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffGetButtonStateCharacteristic(characteristic) {
+    // Add notification hook for button state
+    // (Get notified when button state changes)
+    characteristic.startNotifications().then(() => {
+        characteristic.addEventListener('characteristicvaluechanged', e => {
+            const val = (new Uint8Array(e.target.value.buffer))[0];
+            const el = document.getElementById("heart-rate");
+            el.innerText = val;
+        });
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}

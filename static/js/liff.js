5c21e4d3f258c74b38d8b9ee7c7839d78c644053
Miura55 <acordion.piano@gmail.com>
2019-08-06 07:39:25 +0900

    return default

--- static/js/liff.js
+++ static/js/liff.js
@@ -205,28 +205,3 @@ function liffGetButtonStateCharacteristic(characteristic) {
         uiStatusError(makeErrorMsg(error), false);
     });
 }
-
-// plot graph
-function getData() {
-    return Math.random();
-}
-
-Plotly.plot('chart',[{
-    y:[getData()],
-    type:'line'
-}]);
-
-var cnt = 0;
-
-setInterval(function(){
-
-    Plotly.extendTraces('chart',{ y:[[getData()]]}, [0]);
-    cnt++;
-    if(cnt > 100) {
-        Plotly.relayout('chart',{
-            xaxis: {
-                range: [cnt-100,cnt]
-            }
-        });
-    }
-},15);

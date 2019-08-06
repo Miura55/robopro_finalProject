35f889089ad531546ca4b827df53dc9b1f296a7d
Miura55 <acordion.piano@gmail.com>
2019-08-06 23:52:58 +0900

    Add simple graph

--- static/js/liff.js
+++ static/js/liff.js
@@ -16,6 +16,27 @@ window.onload = () => {
     initializeApp();
 };
 
+// --------------- //
+// Make graph      //
+// --------------- // 
+
+var ctx = document.getElementById("ChartDemo").getContext('2d');
+var ChartDemo = new Chart(ctx, {
+   type: 'line',
+   data: {
+      labels: ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7"],
+      datasets: [
+      {
+         label: "Chart-1",
+         borderColor: 'rgb(255, 0, 0)',
+         data: [20, 26, 12, 43, 33, 21, 29],
+      },
+      ]
+   },
+   options: {
+      responsive: true,
+   }
+});
 
 // ------------ //
 // UI functions //

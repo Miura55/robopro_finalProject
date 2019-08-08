e5b4d39c627e684e8535ff342921217e103829f0
Miura55 <acordion.piano@gmail.com>
2019-08-09 01:25:25 +0900

    Change gauge

--- static/js/liff.js
+++ static/js/liff.js
@@ -19,14 +19,34 @@ window.onload = () => {
 // -------------- //
 // chart function //
 // -------------- //
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(cb) {
+        var i = 0, s = this.length;
+        for (; i < s; i++) {
+            cb && cb(this[i], i, this);
+        }
+    }
+}
+
+document.fonts && document.fonts.forEach(function(font) {
+    font.loaded.then(function() {
+        if (font.family.match(/Led/)) {
+            document.gauges.forEach(function(gauge) {
+                gauge.update();
+            });
+        }
+    });
+});
+
 var timers = [];
 
 function animateGauges() {
     document.gauges.forEach(function(gauge) {
         timers.push(setInterval(function() {
-            gauge.value = Math.random() *
-                (gauge.options.maxValue - gauge.options.minValue) +
-                gauge.options.minValue;
+            var min = gauge.options.minValue - 20;
+            var max = gauge.options.maxValue + 20;
+
+            gauge.value = min + Math.random() * (max - min);
         }, gauge.animation.duration + 50));
     });
 }
@@ -214,7 +234,6 @@ function liffGetButtonStateCharacteristic(characteristic) {
             const val = (new Uint8Array(e.target.value.buffer))[0];
             const el = document.getElementById("temp");
             el.innerText = val;
-            animateGauges();
         });
     }).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
